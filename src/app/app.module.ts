ad7219ced993c79c30ee95f0e8ff04c358eeb1f2
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-06 16:10:31 +0700

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+import { RouterModule, Routes } from "@angular/router";
 
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "servers",
+    component: ServersComponent,
+  },
+  {
+    path: "users",
+    component: UsersComponent,
+  },
+  {
+    path: "users/:id/:name",
+    component: UserComponent,
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,13 +38,15 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
